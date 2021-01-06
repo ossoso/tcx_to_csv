83940013d217c8f1643a66e181ddd6b091f866a5
Ossosoi Loso <lossososoi@gmail.com>
2021-01-06 15:44:34 +0200

    clean imports and comments

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+import base64
+import datetime
+import io
+import re
+import sys
+
+from os.path import basename, splitext
 from collections import OrderedDict
 from itertools import chain
 import dash
@@ -12,12 +19,6 @@ import datashader as ds
 import datashader.transfer_functions as tf
 import pandas as pd
 import numpy as np
-import base64
-import datetime
-import io
-import re
-import sys
-from os.path import basename, splitext
 
 from inspect import getsourcefile
 
@@ -102,17 +103,11 @@ def _parseSelectedJSON(option_id, jsonStr):
     csvBuff = io.StringIO(csvStr)
     return pd.read_csv(csvBuff)
 
-
-def __parseSelectedJSON(option_id, jsonStr):
-    return pd.DataFrame(json.loads(jsonStr)[option_id])
-
 def csvify_dfs(dfDictL):
-    # newL = [{d['filename']: d['df'].to_dict()} for d in dfDictL]
     newL = {filename: df.to_csv() for (filename, df) in dfDictL}
     return json.dumps(newL, ignore_nan=True)
 
 def jsonify_dfs(dfDictL):
-    # newL = [{d['filename']: d['df'].to_dict()} for d in dfDictL]
     newL = {filename: df.to_dict() for (filename, df) in dfDictL}
     return json.dumps(newL, ignore_nan=True)
 
@@ -146,34 +141,6 @@ def _plot_ranges(start, end, signal):
     y_range = (1.2 * signal.min(), 1.2 * signal.max())
     return (x_range, y_range)
 
-# demo data generation
-def _generate_data():
-    n = 1000000
-    max_points = 100000
-
-    np.random.seed(2)
-    cols = ["Signal"]  # Column name of signal
-    start = 1456297053  # Start time
-    end = start + n  # End time
-
-    # Generate a fake signal
-    time = np.linspace(start, end, n)
-    signal = np.random.normal(0, 0.3, size=n).cumsum() + 50
-
-    # Generate many noisy samples from the signal
-    noise = lambda var, bias, n: np.random.normal(bias, var, n)
-    data = {c: signal + noise(1, 10 * (np.random.random() - 0.5), n) for c in cols}
-
-    # # Pick a few samples and really blow them out
-    locs = np.random.choice(n, 10)
-
-    # print locs
-    data["Signal"][locs] *= 2
- 
-    _plot_ranges(start, end, signal)
-    return pd.DataFrame(data)
-# _generate_data()
-
 def power_stats(fit_df):
     t = fit_df[TIME_COLNAME]
     del_t = t.diff()
@@ -185,14 +152,7 @@ def power_stats(fit_df):
     p_avg = energy / duration
     return {'average power (Watt)': p_avg, 'duration (s)': duration}
 
-
-
 max_points = 100000
-
-# read power - t csv
-# df = pd.read_csv('~/hobby_projects/tcx_to_csv/20180111T151701Z_Biking.csv', header=0, skiprows=9)
-
-import pandas as pd
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -210,63 +170,13 @@ def parse_contents(contents, filename, date):
             # Assume that the user uploaded a CSV file
             dfTup = tuple(import_csv(bytestring, filename, src_type="bytestr"))
     except Exception as e:
+        print('error parsing contents')
         raise e
-        print('error at parse_contents\n' +  e)
     return dfTup
 
 
-def exmpl_setup_plot_canvas():
-    n = 1000000
-    max_points = 100000
-
-    np.random.seed(2)
-    cols = ['Signal']  # Column name of signal
-    start = 1456297053  # Start time
-    end = start + n  # End time
-
-    # Generate a fake signal
-    time = np.linspace(start, end, n)
-    signal = np.random.normal(0, 0.3, size=n).cumsum() + 50
-
-    # Generate many noisy samples from the signal
-    noise = lambda var, bias, n: np.random.normal(bias, var, n)
-    data = {c: signal + noise(1, 10 * (np.random.random() - 0.5), n) for c in cols}
-
-    # # Pick a few samples and really blow them out
-    locs = np.random.choice(n, 10)
-
-    # print locs
-    data['Signal'][locs] *= 2
-
-    # # Default plot ranges:
-    x_range = (start, end)
-    y_range = (1.2 * signal.min(), 1.2 * signal.max())
-
-    # Create a dataframe
-    data['Time'] = np.linspace(start, end, n)
-    df = pd.DataFrame(data)
-
-    time_start = df['Time'].values[0]
-    time_end = df['Time'].values[-1]
-
-    # cvs = ds.Canvas(x_range=x_range, y_range=y_range)
-
-    # aggs = OrderedDict((c, cvs.line(df, 'Time', c)) for c in cols)
-    # img = tf.shade(aggs['Signal'])
-
-    # arr = np.array(img)
-    z = arr.tolist()
-
-    # axes
-    dims = len(z[0]), len(z)
-
-    x = np.linspace(x_range[0], x_range[1], dims[0])
-    y = np.linspace(y_range[0], y_range[1], dims[0])
-    return (x, y, z)
-
 def setup_plot_canvas(df):
     cols = [POWER_COLNAME]
-    # data = {c: signal + noise(1, 10 * (np.random.random() - 0.5), n) for c in cols}
     time_start = df[TIME_COLNAME].values[0]
     time_end = df[TIME_COLNAME].values[-1]
 
@@ -290,9 +200,6 @@ def setup_plot_canvas(df):
     return (x, y, z)
 
 
-# if 'df' in vars() or 'df' in globals():
-# x, y, z = setup_plot_canvas()
-# global fig1
 def generateFig(x, y, z, scope):
     fullFig = scope != 'full'
     fig = {
@@ -334,42 +241,6 @@ def generateFig(x, y, z, scope):
     }
     return fig
 
-# global fig2
-# fig2 = {
-#     "data": [
-#         {
-#             "x": x,
-#             "y": y,
-#             "z": z,
-#             "type": "heatmap",
-#             "showscale": False,
-#             "colorscale": [[0, "rgba(255, 255, 255,0)"], [1, "#75baf2"]],
-#         }
-#     ],
-#     "layout": {
-#         "margin": {"t": 50, "b": 20},
-#         "height": 250,
-#         "xaxis": {
-#             "fixedrange": True,
-#             "showline": True,
-#             "zeroline": False,
-#             "showgrid": False,
-#             "showticklabels": True,
-#             "color": "#a3a7b0",
-#         },
-#         "yaxis": {
-#             "fixedrange": True,
-#             "showline": True,
-#             "zeroline": False,
-#             "showgrid": False,
-#             "showticklabels": True,
-#             "ticks": "",
-#             "color": "#a3a7b0",
-#         },
-#         "plot_bgcolor": "#23272c",
-#         "paper_bgcolor": "#23272c",
-#     },
-# }
 
 app.layout = html.Div(
     [
@@ -506,7 +377,6 @@ def draw_undecimated_data(selection, value, jsonStr):
         x0 = selection["xaxis.range[0]"]
         x1 = selection["xaxis.range[1]"]
         sub_df = df[(df[TIME_COLNAME] >= x0) & (df[TIME_COLNAME] <= x1)]
-        num_pts = len(sub_df)
         new_fig1 = fig1.copy()
         high_res_data = [
             dict(
